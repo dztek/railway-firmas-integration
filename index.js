0e2f8606bcbad6008f524aa8337fb9b60337a045
dz <dz@dztek.io>
2023-06-30 13:45:53 -0400

    version

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Choo Choo! Welcome to your Express app 🚅');
+  res.send('Choo Choo! Welcome to your Express app 🚅');
 })
 
+app.get('/version', (req, res) => {
+  res.json({
+    name: 'version',
+    version: process.env.LOGICLOUD_VAR
+  });
+});
+
 app.get("/json", (req, res) => {
-    res.json({"Choo Choo": "Welcome to your Express app 🚅"});
+  res.json({ "Choo Choo": "Welcome to your Express app 🚅" });
 })
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
